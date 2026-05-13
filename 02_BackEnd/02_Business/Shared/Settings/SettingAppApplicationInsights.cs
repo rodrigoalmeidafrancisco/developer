ad5fd0dffaab5baa6173181cb29561223e4b57ec
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2026-05-13 00:35:47 -0300

    Ajustando os parâmetros da aplicação.

---
+++ 02_BackEnd/02_Business/Shared/Settings/SettingAppApplicationInsights.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Settings
+{
+    public class SettingAppApplicationInsights
+    {
+    }
+}
---
+++ 02_BackEnd/02_Business/Shared/Settings/SettingAppParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace Shared.Settings
+{
+    public class SettingAppParameters
+    {
+        public SettingAppParameters()
+        {
+            Proxy = new SettingAppParametersProxy();
+        }
+
+        public SettingAppParametersProxy Proxy { get; set; }
+    }
+
+    public class SettingAppParametersProxy
+    {
+        public string ByPass { get; set; }
+        public string[] ByPassArray => ByPass.Split('|');
+        public string Enable { get; set; }
+        public string Porta { get; set; }
+        public string Url { get; set; }
+        public string UrlPorta => $"{Url}:{Porta}";
+    }
+}
